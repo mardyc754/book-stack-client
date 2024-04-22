24e038646ba0d7ae197beb6c9369f4cab129ccc3
Marta Dychała <mar754dyc@gmail.com>
2024-04-22 21:55:11 +0200

    Install chakra ui (#1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import {
+  ChakraProvider
+  // extendTheme
+} from '@chakra-ui/react';
+
 import './index.css';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+// const colors = {
+//   brand: {
+//     900: '#1a365d',
+//     800: '#153e75',
+//     700: '#2a69ac',
+//   },
+// }
+
+// const theme = extendTheme({ colors })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      {/* <ChakraProvider theme={theme}> */}
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
