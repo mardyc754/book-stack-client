164e45b4534c12aa32b2ce95efa479659d902314
Marta Dychała <mar754dyc@gmail.com>
2024-05-03 19:17:28 +0200

    Install DaisyUI, remove Chakra UI and its dependencies (#1)

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,11 @@
-// import {
-//   ChakraProvider // extendTheme
-// } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './index.css';
 
-// 2. Extend the theme to include custom colors, fonts, etc
-// const colors = {
-//   brand: {
-//     900: '#1a365d',
-//     800: '#153e75',
-//     700: '#2a69ac',
-//   },
-// }
-
-// const theme = extendTheme({ colors })
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <ChakraProvider> */}
-    {/* <ChakraProvider theme={theme}> */}
     <App />
-    {/* </ChakraProvider> */}
   </React.StrictMode>
 );
